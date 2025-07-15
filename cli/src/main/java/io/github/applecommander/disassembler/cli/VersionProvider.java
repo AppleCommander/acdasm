538db1b516b6f0e97470ce4203a324b0b6f0051d
Rob Greene <robgreene@gmail.com>
2025-07-15 16:05:46 -0500

    Pulling version from API instead of CLI project.

--- cli/src/main/java/io/github/applecommander/disassembler/cli/VersionProvider.java
+++ cli/src/main/java/io/github/applecommander/disassembler/cli/VersionProvider.java
@@ -1,12 +1,13 @@
 package io.github.applecommander.disassembler.cli;
 
+import io.github.applecommander.disassembler.api.Disassembler;
 import picocli.CommandLine.IVersionProvider;
 
 /** Display version information.  Note that this is dependent on the Spring Boot Gradle plugin configuration. */
 public class VersionProvider implements IVersionProvider {
     public String[] getVersion() {
     	return new String[] { 
-            String.format("acdasm: %s", Main.class.getPackage().getImplementationVersion()),
+            String.format("acdasm: %s", Disassembler.class.getPackage().getImplementationVersion()),
 		};
     }
 }
